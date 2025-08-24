4a58a0954ff011f1c3575d1a5a0a10782e43200b
{shaurya shukla} <shauryaixd7537@gmail.com>
2025-08-24 15:41:03 +0530

    13

---
+++ leetcode/344.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int st = 0;
+        int e = s.size() - 1;
+
+        while(st<e){
+            swap(s[st++],s[e--]);
+        }
+    }
+};
+
+//or
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+    }
+};
