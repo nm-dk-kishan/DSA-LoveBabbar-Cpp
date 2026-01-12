768b4f8301b55a970ab4f12c3d01d3e5a5df11f6
{shaurya shukla} <shauryaixd7537@gmail.com>
2026-01-12 23:33:47 +0530

    32

--- lectures/lecture-43.cpp
+++ lectures/lecture-43.cpp
@@ -1,46 +1,86 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class human{
+// class Human {
 
-  public:
-    int height;
-    int weight;
-    int age;
+//   //Implementation details
+//   public:
+//   int height;
+//   int age;
+//   int weight;
 
-    public:
-    int getAge(){
-      return this->age;
-    }
+//   public:
+//   int getAge(){
+//     return this->age;
+//   }
 
-    void setWeight(int w){
-      this->weight = w;
-    }
+//   void setWeight(int w){
+//     this->weight = w;
+//   }
+// };
 
+// class Male:public Human{
+
+//   public:
+//   string color;
+
+//   void sleep() {
+//     cout << "Male is sleeping" << endl;
+//   }
+// };
+
+// Multi-level Inheritance + Multiple Inheritance 
+
+class A{
+  public: 
+  void funcA(){
+    cout << "Inside func A" << endl;
+  }
 };
 
-class male: public human{
-  public:
-    string color;
+class B{
+  public: 
+  void funcB(){
+    cout << "Inside func B" << endl;
+  }
+};
 
-  void sleep(){
-    cout << "male is sleeping" << endl;
+// class C: public B{
+//   public: 
+//   void funcC(){
+//     cout << "Inside func C" << endl;
+//   }
+// };
+
+class D: public A,public B {
+  public: 
+  void funcD(){
+    cout << "Inside func D" << endl;
   }
 };
 
 int main(){
 
-  male object1;
+  // C objC;
+  // objC.funcA();
 
-  cout << object1.height << endl;
-  cout << object1.weight << endl;
-  cout << object1.age << endl;
+  D objD;
+  objD.funcA();
+  objD.funcB();
 
-  cout << object1.color << endl;
+  // Male obj;
+  
+  // cout << obj.age << endl;
+  // cout << obj.height << endl;
+  // cout << obj.weight << endl;
 
-  object1.setWeight(84);
-  cout << object1.weight << endl;
-  object1.sleep();
+  // cout << obj.color << endl;
+
+  // obj.setWeight(38);
+
+  // cout << obj.weight << endl;
+
+  // obj.sleep();
 
   return 0;
 };
--- lectures/tempCodeRunnerFile.cpp
+++ lectures/tempCodeRunnerFile.cpp
@@ -1,5 +1,2 @@
-nt arr[6] = {4,2,1,1,3,5};
-  // SelectionSort(arr,6);
-  // for(int i =0;i<6;i++){
-  //   cout << arr[i] << " ";
-  // }
+jC;
+  // objC.funcA();
