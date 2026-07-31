ec25c28a7593e6e44b37270caafbca7f561b6c35
Shaurya Shukla <shauryaixd7537@gmail.com>
2026-07-31 22:58:06 +0530

    54

---
+++ GFG/Zero Sum Subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool subArrayExists(vector<int>& arr) {
+        unordered_set<int> st;
+        long long prefixSum =0;
+
+        for(int x:arr){
+            prefixSum+=x;
+
+            if(prefixSum==0){
+                return true;
+            }
+
+            if(st.count(prefixSum)){
+                return true;
+            }
+
+            st.insert(prefixSum);
+        }
+        return false
+    }
