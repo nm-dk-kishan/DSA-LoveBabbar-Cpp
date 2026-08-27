c38154482d4a57ea400e2cf22ee35cb2ca06ef04
Shaurya Shukla <shauryaixd7537@gmail.com>
2026-08-27 22:46:45 +0530

    73

---
+++ GFG/Palindrome String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+        int i =0;
+        int j =s.size()-1;
+        
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+    }
+};
